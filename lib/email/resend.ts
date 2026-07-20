82908a280061534977046faccf141b1daacd0d6d
ayandamazengwework-commits <ayandamazengwework@gmail.com>
2026-07-20 14:13:29 +0200

    Update resend.ts

--- lib/email/resend.ts
+++ lib/email/resend.ts
@@ -7,8 +7,6 @@ export function getResend() {
 
   const apiKey = process.env.RESEND_API_KEY
 
-  // Don't crash the build if the env variable doesn't exist yet.
-  // Only throw when an email is actually being sent.
   if (!apiKey) {
     throw new Error('Missing RESEND_API_KEY')
   }
