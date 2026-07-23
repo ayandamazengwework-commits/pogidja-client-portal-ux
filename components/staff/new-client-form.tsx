19952ea8c5c1ea161ff10a294f0d9afa39deb68e
ayandamazengwework-commits <ayandamazengwework@gmail.com>
2026-07-23 15:15:47 +0200

    Update new-client-form.tsx

--- components/staff/new-client-form.tsx
+++ components/staff/new-client-form.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react'
 import { createClientProfile } from '@/app/staff/clients/new/actions'
 
 import { Button } from '@/components/ui/button'
-import { Card, { CardContent } from '@/components/ui/card'
+import { Card, CardContent } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Textarea } from '@/components/ui/textarea'
