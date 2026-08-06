2c460e05a66847669b15d9ad66726f4c88b94fac
ayandamazengwework-commits <ayandamazengwework@gmail.com>
2026-08-06 10:46:53 +0200

    Update transporter.ts

--- lib/email/transporter.ts
+++ lib/email/transporter.ts
@@ -1,13 +1,17 @@
-import nodemailer from "nodemailer";
+import nodemailer from 'nodemailer'
+
+console.log('HOST:', process.env.SMTP_HOST)
+console.log('PORT:', process.env.SMTP_PORT)
+console.log('USER:', process.env.SMTP_USER)
+console.log('PASS EXISTS:', !!process.env.SMTP_PASS)
+console.log('FROM:', process.env.SMTP_FROM)
 
 export const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST!,
-  port: Number(process.env.SMTP_PORT!),
-
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
   secure: Number(process.env.SMTP_PORT) === 465,
-
   auth: {
-    user: process.env.SMTP_USER!,
-    pass: process.env.SMTP_PASS!,
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
-});
+})
